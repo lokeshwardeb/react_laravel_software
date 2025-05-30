9d76c7fb6d7a586e5ac52dc2dce2b938967270f2
Lokeshwar Deb Protik <lokeshwardebdevelop@gmail.com>
2025-05-31 04:17:42 +0600

    added the laravel breeze and initializing the version 2.0.0

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,17 +1,28 @@
+import '../css/app.css';
 import './bootstrap';
 
-import '../css/app.css';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
-
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`]
-  },
-  setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
-  },
-})
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    setup({ el, App, props }) {
+        if (import.meta.env.SSR) {
+            hydrateRoot(el, <App {...props} />);
+            return;
+        }
+
+        createRoot(el).render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
